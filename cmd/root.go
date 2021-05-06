9365b72f1e1673b58bd789daffcf05c2f88480f7
Jelle van der Waa <jelle@vdwaa.nl>
2021-05-06 14:37:41 +0200

    Add Use/Short/Long information for cobra

--- cmd/root.go
+++ cmd/root.go
@@ -8,6 +8,10 @@ import (
 
 // RootCmd provides the commandline parser root.
 var RootCmd = &cobra.Command{
+	Use: "sogrep-service",
+	Short: "Sogrep service is a sogrep HTTP API",
+	Long: `Sogrep service parses Arch Linux's links databases and
+provides a simple HTTP JSON API to find out which packages require a given soname.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		cmd.Help()
 		os.Exit(2)
